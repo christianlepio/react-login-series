34910e4e733091365ba4d9870644464f99ac133d
christianlepio <rylepio79@gmail.com>
2023-12-01 20:27:53 +0800

    connect register form to backend (node express JS & mongoDB)

--- 1-react-form_validation/src/components/Register.jsx
+++ 1-react-form_validation/src/components/Register.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from "react"
+import axios from "../api/axios"
 
 // [a-zA-Z] : must start with lower or uppercase letter
 // [a-zA-Z0-9-_]{3,23}$ : must followed by 3-23 characters that is lower/uppercase, digits, hypens, or underscore
@@ -7,6 +8,7 @@ const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/
 // password value hould be atleast 8-24 characters that
 // requires atleast 1 lower & uppercase letters, digit, and special char
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/
+const REGISTER_URL = '/register' //endpoint url for backend api (nodeJS: 14-Data-Models)
 
 const Register = () => {
     const userRef = useRef()
@@ -65,120 +67,181 @@ const Register = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault()
+        //if button enabled with JS hack 
+        //validate again the user and password
+        const v1 = USER_REGEX.test(user)
+        const v2 = PWD_REGEX.test(pwd)
+        const v3 = pwd === matchPwd
+
+        if (!v3) {
+            console.log('v3: ', v3)
+            setErrMsg('Password not matched!')
+            return
+        }
+        if (!v1 || !v2) {
+            console.log('v1: ', v1, 'v2: ', v2)
+            setErrMsg('Invalid username or password!')
+            return
+        }
+
+        try {
+            const response = await axios.post(REGISTER_URL, 
+                //provide the payload (data you are sending)
+                //user and pwd is what our backend is expecting a exact property name
+                JSON.stringify({ user, pwd }),
+                //third parameter here is an object that is required to post in backend
+                {
+                    headers: { 'Content-Type': 'application/json' },
+                    withCredentials: true //this is required for CORS
+                }
+            )
+
+            console.log('response.data: ', response.data)
+            console.log('response.accessToken: ', response.accessToken)
+            console.log('JSON.stringify(response): ',JSON.stringify(response))
+
+            setSuccess(true)
+
+            //clear input fields here
+
+        } catch (err) {
+            if (!err?.response) {
+                setErrMsg('No Server Response!')
+            } else if (err.response?.status === 409) {
+                setErrMsg('Username already exists!')
+            } else {
+                setErrMsg('Registration Failed')
+            }
+            //focus screen on error msg
+            errRef.current.focus()
+        }
     }
 
     return (
-        <section className="px-4 py-4 shadow border rounded-4 align-self-center">
-            {errMsg ? 
-                <div ref={errRef} className="alert alert-danger alert-dismissible fade show" role="alert">
-                    <strong>{errMsg}</strong>
-                    <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
-                : null
+        <>
+            {success ? 
+                (
+                    <section className="px-4 py-4 shadow border rounded-4 align-self-center">
+                        <h1 className="h1 mb-4 mt-2 text-center">You have successfully registered your account!</h1>
+                        <p className="text-center fs-6 mt-3 lead">Go and <a href="#">Sign In</a> now!</p>
+                    </section>
+                ) : (
+                
+                    <section className="px-4 py-4 shadow border rounded-4 align-self-center">
+                        <div 
+                            ref={errRef} 
+                            className={"alert alert-danger " + (errMsg ? null : 'd-none')} 
+                            role="alert" 
+                        >
+                            <div><i className="bi bi-exclamation-circle me-2"></i><strong>{errMsg}</strong></div>
+                        </div>
+
+                        <h1 className="h1 mb-4 mt-2 text-center">R<u>EGISTRATION FOR</u>M</h1>
+
+                        <form onSubmit={handleSubmit}>
+                            <div className="mb-3">
+                                <label htmlFor="usernameInput" className="form-label lead">Username</label>
+                                <input 
+                                    type="text" 
+                                    id="usernameInput"
+                                    placeholder="Enter username here:"
+                                    className={"form-control mb-1 " + (user ? validName ? 'is-valid' : 'is-invalid' : null)}
+                                    ref={userRef}
+                                    autoComplete="off" //to avoid auto suggestion of values from the input
+                                    value={user}
+                                    onChange={(e) => setUser(e.target.value)}
+                                    onFocus={() => setUserFocus(true)} //this will be the indicator if the screen is focused on username input
+                                    onBlur={() => setUserFocus(false)} //this will be the indicator if the screen is not focused on username input
+                                    required
+                                />
+                                <div className={userFocus ? user ? validName ? 'valid-feedback' : 'invalid-feedback' : null : null}>
+                                    {userFocus 
+                                        ? user 
+                                            ? validName
+                                                ? ( <p>Username looks good!</p> )
+                                                : ( <p>
+                                                        Username should be at least 4 to 24 characters long. <br/> 
+                                                        Must begin with a letter. <br/>  
+                                                        Numbers, underscores, hypens are allowed. 
+                                                    </p> )
+                                            : null 
+                                        : null
+                                    }
+                                </div>
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="pwdInput" className="form-label lead">Password</label>
+                                <input 
+                                    type="password" 
+                                    id="pwdInput"
+                                    placeholder="Enter password here:" 
+                                    className={"form-control mb-1 " + (pwd ? validPwd ? 'is-valid' : 'is-invalid' : null)}
+                                    value={pwd}
+                                    onChange={(e) => setPwd(e.target.value)}
+                                    onFocus={() => setPwdFocus(true)}
+                                    onBlur={() => setPwdFocus(false)}
+                                    required
+                                />
+                                <div className={pwdFocus ? pwd ? validPwd ? 'valid-feedback' : 'invalid-feedback' : null : null}>
+                                    {pwdFocus 
+                                        ? pwd 
+                                            ? validPwd
+                                                ? ( <p>Password looks good!</p> )
+                                                : ( <p>
+                                                        Password should be at least 8 to 24 characters long. <br/> 
+                                                        Must include upper & lower case, a number, and <br/>
+                                                        special characters. <br/>
+                                                        Allowed special characters: ! @ # $ %  
+                                                    </p> )
+                                            : null 
+                                        : null
+                                    }
+                                </div>
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="confirmPwd" className="form-label lead">Confirm Password</label>
+                                <input 
+                                    type="password" 
+                                    id="confirmPwd"
+                                    placeholder="Re-type password here:" 
+                                    className={"form-control " + (matchPwd ? validMatch ? 'is-valid' : 'is-invalid' : null)}
+                                    value={matchPwd}
+                                    onChange={(e) => setMatchPwd(e.target.value)}
+                                    onFocus={() => setMatchFocus(true)}
+                                    onBlur={() => setMatchFocus(false)}
+                                    required
+                                />
+                                <div className={matchFocus ? matchPwd ? validMatch ? 'valid-feedback' : 'invalid-feedback' : null : null}>
+                                    {matchFocus 
+                                        ? matchPwd 
+                                            ? validMatch
+                                                ? ( <p>Password matched!</p> )
+                                                : ( <p>Password not matched.</p> )
+                                            : null 
+                                        : null
+                                    }
+                                </div>
+                            </div>
+                            <div className="d-flex">
+                                <button 
+                                    type="submit"
+                                    className="btn btn-success flex-grow-1 mt-2"
+                                    disabled={!canSignIn}
+                                >
+                                    Sign Up
+                                </button>
+                            </div>
+                        </form>
+                        
+                        <p className="text-center fs-6 mt-3 lead">
+                            Already registered? <br />
+                            <a href="#">Sign In</a>
+                        </p>
+
+                    </section>
+                )
             }
-
-            <h1 className="fs-1 mb-4 mt-2 text-center">REGISTRATION FORM</h1>
-
-            <form onSubmit={handleSubmit}>
-                <div className="mb-3">
-                    <label htmlFor="usernameInput" className="form-label">Username</label>
-                    <input 
-                        type="text" 
-                        id="usernameInput"
-                        placeholder="Enter username here:"
-                        className={"form-control mb-1 " + (user ? validName ? 'is-valid' : 'is-invalid' : null)}
-                        ref={userRef}
-                        autoComplete="off" //to avoid auto suggestion of values from the input
-                        value={user}
-                        onChange={(e) => setUser(e.target.value)}
-                        onFocus={() => setUserFocus(true)} //this will be the indicator if the screen is focused on username input
-                        onBlur={() => setUserFocus(false)} //this will be the indicator if the screen is not focused on username input
-                        required
-                    />
-                    <div className={userFocus ? user ? validName ? 'valid-feedback' : 'invalid-feedback' : null : null}>
-                        {userFocus 
-                            ? user 
-                                ? validName
-                                    ? ( <p>Username looks good!</p> )
-                                    : ( <p>
-                                            Username should be at least 4 to 24 characters long. <br/> 
-                                            Must begin with a letter. <br/>  
-                                            Numbers, underscores, hypens are allowed. 
-                                        </p> )
-                                : null 
-                            : null
-                        }
-                    </div>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="pwdInput" className="form-label">Password</label>
-                    <input 
-                        type="password" 
-                        id="pwdInput"
-                        placeholder="Enter password here:" 
-                        className={"form-control mb-1 " + (pwd ? validPwd ? 'is-valid' : 'is-invalid' : null)}
-                        value={pwd}
-                        onChange={(e) => setPwd(e.target.value)}
-                        onFocus={() => setPwdFocus(true)}
-                        onBlur={() => setPwdFocus(false)}
-                        required
-                    />
-                    <div className={pwdFocus ? pwd ? validPwd ? 'valid-feedback' : 'invalid-feedback' : null : null}>
-                        {pwdFocus 
-                            ? pwd 
-                                ? validPwd
-                                    ? ( <p>Password looks good!</p> )
-                                    : ( <p>
-                                            Password should be at least 8 to 24 characters long. <br/> 
-                                            Must include upper & lower case, a number, and <br/>
-                                            special characters. <br/>
-                                            Allowed special characters: ! @ # $ %  
-                                        </p> )
-                                : null 
-                            : null
-                        }
-                    </div>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="confirmPwd" className="form-label">Confirm Password</label>
-                    <input 
-                        type="password" 
-                        id="confirmPwd"
-                        placeholder="Re-type password here:" 
-                        className={"form-control " + (matchPwd ? validMatch ? 'is-valid' : 'is-invalid' : null)}
-                        value={matchPwd}
-                        onChange={(e) => setMatchPwd(e.target.value)}
-                        onFocus={() => setMatchFocus(true)}
-                        onBlur={() => setMatchFocus(false)}
-                        required
-                    />
-                    <div className={matchFocus ? matchPwd ? validMatch ? 'valid-feedback' : 'invalid-feedback' : null : null}>
-                        {matchFocus 
-                            ? matchPwd 
-                                ? validMatch
-                                    ? ( <p>Password matched!</p> )
-                                    : ( <p>Password not matched.</p> )
-                                : null 
-                            : null
-                        }
-                    </div>
-                </div>
-                <div className="d-flex">
-                    <button 
-                        className="btn btn-success flex-grow-1 mt-2"
-                        disabled={!canSignIn}
-                    >
-                        Sign Up
-                    </button>
-                </div>
-            </form>
-            
-            <p className="text-center fs-6 mt-3">
-                Already registered? <br />
-                <a href="#">Sign In</a>
-            </p>
-
-        </section>
+        </>
     )
 }
 
