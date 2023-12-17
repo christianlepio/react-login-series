932eea240c5eebbb5c3765d29127d4d6e68173b9
christianlepio <rylepio79@gmail.com>
2023-12-18 02:04:35 +0800

    update welcome page

---
+++ 8-redux-jwt-rt-cookies-flow/src/features/auth/components/Welcome.jsx
@@ -0,0 +1,11 @@
+import { useSelector } from 'react-redux'
+import { selectCurrentUser, selectCurrentToken } from '../authSlice'
+import { Link } from 'react-router-dom'
+
+const Welcome = () => {
+    return (
+        <div>Welcome</div>
+    )
+}
+
+export default Welcome
