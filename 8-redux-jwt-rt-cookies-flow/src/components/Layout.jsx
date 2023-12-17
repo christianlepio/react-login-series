059ac6f3f7f4b0394846288e6e6b960a29b1b8e5
christianlepio <rylepio79@gmail.com>
2023-12-18 01:59:50 +0800

    update login form

--- 8-redux-jwt-rt-cookies-flow/src/components/Layout.jsx
+++ 8-redux-jwt-rt-cookies-flow/src/components/Layout.jsx
@@ -1,7 +1,11 @@
 import { Outlet } from "react-router-dom"
 
 const Layout = () => {
-    return <Outlet />
+    return (
+        <main className="d-flex justify-content-center">
+            <Outlet />
+        </main>
+    )
 }
 
 export default Layout
--- 8-redux-jwt-rt-cookies-flow/src/features/auth/components/Login.jsx
+++ 8-redux-jwt-rt-cookies-flow/src/features/auth/components/Login.jsx
@@ -54,14 +54,61 @@ const Login = () => {
     const handleUserInput = (e) => setUser(e.target.value)
     const handlePwdInput = (e) => setPwd(e.target.value)
 
+    //check if username, password has a value.
+    //this will return either true or false for disabling sign in button
+    const canSignIn = [user, pwd].every(Boolean)
+
     const content = isLoading 
         ? <h1 className="text-center h1 mb-5">Loading...</h1>
         : (
-            <section>
-                {errMsg
-                    ? <p>{errMsg}</p>
-                    : null
-                }
+            <section className="px-4 py-4 mx-4 shadow border rounded-4 align-self-center">
+                <div 
+                    ref={errRef} 
+                    className={"alert alert-danger " + (errMsg ? null : 'd-none')}
+                    role="alert"
+                >
+                    <div><i className="bi bi-exclamation-circle me-2"></i><strong>{errMsg}</strong></div>
+                </div>
+
+                <h1 className="h1 mb-4 mt-2 text-center">E<u>MPLOYEE SIGN I</u>N</h1>
+
+                <form onSubmit={handleSubmit} className="mx-4">
+                    <div className="mb-3">
+                        <label htmlFor="usernameInput" className="form-label lead">Username</label>
+                        <input 
+                            type="text" 
+                            id="usernameInput"
+                            placeholder="Enter username here:"
+                            className="form-control form-control-lg mb-1"
+                            ref={userRef}
+                            value={user}
+                            onChange={handleUserInput}
+                            autoComplete="off" //to avoid auto suggestion of values from the input
+                            required
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="pwdInput" className="form-label lead">Password</label>
+                        <input 
+                            type="password" 
+                            id="pwdInput"
+                            placeholder="Enter password here:" 
+                            className="form-control form-control-lg mb-1"
+                            value={pwd}
+                            onChange={handlePwdInput}
+                            required
+                        />
+                    </div>
+                    <div className="d-flex">
+                        <button 
+                            type="submit"
+                            className="btn btn-lg btn-success flex-grow-1 mt-2"
+                            disabled={!canSignIn}
+                        >
+                            Sign In
+                        </button>
+                    </div>
+                </form>
             </section>
         )
 
