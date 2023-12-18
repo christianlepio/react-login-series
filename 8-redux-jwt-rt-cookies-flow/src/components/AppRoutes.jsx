3c8c1403a4f99be8b2c7ef20f3461ebeddc5c73e
christianlepio <rylepio79@gmail.com>
2023-12-19 02:34:37 +0800

    provide app routes

--- 8-redux-jwt-rt-cookies-flow/src/components/AppRoutes.jsx
+++ 8-redux-jwt-rt-cookies-flow/src/components/AppRoutes.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+//react router dom
+import { Routes, Route } from "react-router-dom"
+//components
+import Layout from "./Layout"
+import InitialPage from "./InitialPage"
+import Login from "../features/auth/components/Login"
+import Welcome from "../features/auth/components/Welcome"
+import RequireAuth from "../features/auth/components/RequireAuth"
 
 const AppRoutes = () => {
     return (
-        <div>AppRoutes</div>
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                {/* public routes here */}
+                <Route index element={<InitialPage />} />
+                <Route path="login" element={<Login />} />
+
+                {/* protected routes here */}
+                <Route element={<RequireAuth />}>
+                    <Route path="welcome" element={<Welcome />} />
+                </Route>
+            </Route>
+        </Routes>
     )
 }
 
