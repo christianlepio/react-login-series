9d42cd27fadbab60507d24abec2a89b047f6a142
christianlepio <rylepio79@gmail.com>
2023-12-19 03:34:36 +0800

    add index css for main html element

--- 8-redux-jwt-rt-cookies-flow/src/index.jsx
+++ 8-redux-jwt-rt-cookies-flow/src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 //CSS
+import './index.css'
 import 'bootstrap/dist/css/bootstrap.css' //import bootstrap css
 import 'bootstrap-icons/font/bootstrap-icons.css' //import bootstrap icon
 import 'bootstrap/dist/js/bootstrap.bundle.js' //import bootstrap js
