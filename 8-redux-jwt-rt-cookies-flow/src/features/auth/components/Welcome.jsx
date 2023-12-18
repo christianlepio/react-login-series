cc0ba79b81ec00938c794242a162fe4370fa4cc4
christianlepio <rylepio79@gmail.com>
2023-12-19 02:12:44 +0800

    update welcome page content

--- 8-redux-jwt-rt-cookies-flow/src/features/auth/components/Welcome.jsx
+++ 8-redux-jwt-rt-cookies-flow/src/features/auth/components/Welcome.jsx
@@ -3,9 +3,21 @@ import { selectCurrentUser, selectCurrentToken } from '../authSlice'
 import { Link } from 'react-router-dom'
 
 const Welcome = () => {
-    return (
-        <div>Welcome</div>
+    const user = useSelector(selectCurrentUser)
+    const token = useSelector(selectCurrentToken)
+
+    const welcome = user ? `Welcome ${user}` : 'Welcome'
+    const tokenAbbr = `${token.slice(0, 9)}...`
+
+    const content = (
+        <section className="px-4 py-4 mx-4 shadow border rounded-4 align-self-center">
+            <h1 className="h1 mb-4 mt-2 text-center">{welcome}</h1>
+            <p className="lead mb-4 text-center">{tokenAbbr}</p>
+            <p className="lead mb-3">Go to <Link to='/userslist'>User's</Link> list</p>
+        </section>
     )
+
+    return content
 }
 
 export default Welcome
